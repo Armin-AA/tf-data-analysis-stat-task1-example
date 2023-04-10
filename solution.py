1f456dd37587de46384236997fad54d7be3ec039
Armin-AA <aat6@yandex.ru>
2023-04-10 11:31:04 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,6 +5,5 @@ import numpy as np
 chat_id = 98268891 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    
-    a = np.random.normal(-21, np.exp(1), len(x))-x/(10*10)
-    return a.mean() # Ваш ответ  
+ 
+    return (x.mean()+20)/10 # Ваш ответ  
